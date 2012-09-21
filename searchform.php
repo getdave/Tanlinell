b3debb8cf8b91e8cf7d20c8b5d17668df4092216
David <david@burfieldcreative.co.uk>
2012-09-21 17:15:17 +0100

    Semantic and class naming amends to site search file

--- searchform.php
+++ searchform.php
@@ -6,8 +6,8 @@
  * @since Tanlinell 1.0
  */
 ?>
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-		<label for="s" class="assistive-text"><?php _e( 'Search', 'tanlinell' ); ?></label>
-		<input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'tanlinell' ); ?>" />
-		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'tanlinell' ); ?>" />
+	<form method="get" class="site-search" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+		<label for="site-search-search-field" class="site-search-label assistive-text"><?php _e( 'Search', 'tanlinell' ); ?></label>
+		<input type="search" class="site-search-input" name="site-search-search-field" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'tanlinell' ); ?>" />
+		<input type="submit" class="site-search-submit" name="submit" value="<?php esc_attr_e( 'Search', 'tanlinell' ); ?>" />
 	</form>
