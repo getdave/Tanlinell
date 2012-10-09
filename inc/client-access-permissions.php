a97163b53ac470f8c233cc3008b98109438c7638
sanjaysparx <sanjay.kumar@sparxtechnologies.com>
2012-10-09 11:54:50 +0530

    Added 'client-access-permissions.php & register-custom-posts.php'

---
+++ inc/client-access-permissions.php
@@ -0,0 +1,53 @@
+<?php 
+/**
+ * Customize the Access permission for the client
+ *
+ *
+ * @package Tanlinell
+ * @since Tanlinell 1.0
+ */
+
+/**
+ * 	Adding a user role: tanlinell_admin
+ */
+
+add_role( 'tanlinell_admin', 'tanlinell admin' );
+
+/**
+ * 	Removing action buttons for the User Role : tanlinell_admin 
+ * 
+ * 	Disabling action buttons for the admin
+ * 
+ */
+	
+if( current_user_can( 'tanlinell_admin' ) ) {
+	
+	$prefix = $screen->is_network ? 'network_admin_' : '';
+	
+	// Plugin page action buttons
+	add_filter( $prefix .'plugin_action_links', 'disable_plugin_deactivation', 10, 4 ); 
+	add_filter('bulk_actions-plugins','plugin_page_bulk_actions');
+	
+}
+
+function disable_plugin_deactivation( $actions, $plugin_file, $plugin_data, $context ) {
+		// Remove edit link
+		if ( array_key_exists( 'edit', $actions ) )
+			unset( $actions['edit'] );
+		// Remove deactivate link 
+		if ( array_key_exists( 'deactivate', $actions ) )
+			unset( $actions['deactivate'] );
+		// Remove activate link
+		if ( array_key_exists( 'activate', $actions ) )
+			unset( $actions['activate'] );
+		
+		return $actions;
+}
+
+function plugin_page_bulk_actions($actions){
+	
+	unset( $actions['activate-selected'] );
+	unset( $actions['deactivate-selected'] );
+	
+	return $actions;
+}
---
+++ inc/register-custom-posts.php
@@ -0,0 +1,52 @@
+<?php 
+/**
+ * Register custom posts type for the theme
+ *
+ *
+ * @package Tanlinell
+ * @since Tanlinell 1.0
+ */
+
+/**
+ * Homepage slider - post_type: "slider"
+ */
+
+add_action( 'init', 'register_cpt_slide' );
+
+function register_cpt_slide() {
+
+	$labels = array(
+			'name' => _x( 'Slides', 'slide' ),
+			'singular_name' => _x( 'Slide', 'slide' ),
+			'add_new' => _x( 'Add New', 'slide' ),
+			'add_new_item' => _x( 'Add New Slide', 'slide' ),
+			'edit_item' => _x( 'Edit Slide', 'slide' ),
+			'new_item' => _x( 'New Slide', 'slide' ),
+			'view_item' => _x( 'View Slide', 'slide' ),
+			'search_items' => _x( 'Search Slides', 'slide' ),
+			'not_found' => _x( 'No slides found', 'slide' ),
+			'not_found_in_trash' => _x( 'No slides found in Trash', 'slide' ),
+			'parent_item_colon' => _x( 'Parent Slide:', 'slide' ),
+			'menu_name' => _x( 'Homepage Slider', 'slide' ),
+	);
+
+	$args = array(
+			'labels' => $labels,
+			'hierarchical' => true,
+			'supports' => array( 'title', 'editor', 'thumbnail', 'page-attributes' ),
+			'public' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'show_in_nav_menus' => true,
+			'publicly_queryable' => true,
+			'exclude_from_search' => false,
+			'has_archive' => true,
+			'query_var' => true,
+			'can_export' => true,
+			'rewrite' => true,
+			'capability_type' => 'post'
+	);
+
+	register_post_type( 'slide', $args );
+	flush_rewrite_rules();
+}
