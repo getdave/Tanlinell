9ede4a93832b393a0d2508396e67bd16beba10ea
David <getdavemail@gmail.com>
2012-11-26 10:19:04 +0000

    Including shortcodes by default. Commenting and whitespace alterations

--- functions.php
+++ functions.php
@@ -6,13 +6,21 @@
  * @since Tanlinell 1.0
  */
 
+
+
+
 /**
  * Set the content width based on the theme's design and stylesheet.
  *
  * @since Tanlinell 1.0
  */
-if ( ! isset( $content_width ) )
+if ( ! isset( $content_width ) ) {
 	$content_width = 640; /* pixels */
+}
+
+
+
+
 
 if ( ! function_exists( 'tanlinell_setup' ) ):
 /**
@@ -59,16 +67,8 @@ function tanlinell_setup() {
 	 * Custom Shortcodes
 	 * add your own custom shortcodes into this file
 	 */	
-	//require( get_template_directory() . '/inc/custom-shortcodes.php' );
+	require( get_template_directory() . '/inc/custom-shortcodes.php' );
 	
-
-
-	/**
-	 * Custom Widgets
-	 * add your own custom Widgets into this file
-	 */	
-	//require( get_template_directory() . '/inc/custom-widgets.php' );
-
 
 
 	/**
@@ -86,6 +86,12 @@ function tanlinell_setup() {
 	require( get_template_directory() . '/inc/register-nav-menus.php' );
 
 
+	/**
+	 * Custom Widgets
+	 * add your own custom Widgets into this file
+	 */	
+	//require( get_template_directory() . '/inc/custom-widgets.php' );
+	
 
 	/**
 	 * Register Widgets
@@ -94,18 +100,17 @@ function tanlinell_setup() {
 	require( get_template_directory() . '/inc/register-widget-areas.php' );
 
 
+	/**
+	 * 	Register 'Custom Posts Types' for the theme
+	 */
+	require( get_template_directory() . '/inc/register-custom-posts.php' );
+
 	
 	/**
 	 * Enqueue Scripts & CSS Styles
 	 * adds javascripts and stylesheets the right way via WP enqueue
 	 */
 	require( get_template_directory() . '/inc/enqueue-scripts-styles.php' );
-	
-	
-	/**
-	 * 	Register 'Custom Posts Types' for the theme
-	 */
-	require( get_template_directory() . '/inc/register-custom-posts.php' );
 	
 	
 	/**
