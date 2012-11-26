7c2107802e62201c8cd466dab795c03fac5ad3a8
David <getdavemail@gmail.com>
2012-11-26 11:09:54 +0000

    Added Hybrid Core Features init into add-theme-support

--- inc/add-theme-support.php
+++ inc/add-theme-support.php
@@ -9,6 +9,12 @@
 
 
 /**
+ * WORDPRESS CORE FEATURES
+ *
+ * Initalizes support for features available within WordPress core
+ */
+
+/**
  * Add default posts and comments RSS feed links to head
  */
 add_theme_support( 'automatic-feed-links' );
@@ -35,5 +41,36 @@ add_theme_support( 'post-formats', array(
 	//'quote', 
 	//'image' 
 ));
+
+
+
+
+/**
+ * HYBRID CORE FEATURS
+ *
+ * Initalizes support for Hybrid Core features. These are listed at:
+ * http://themehybrid.com/hybrid-core
+ */
+
+/* Add theme support for core framework features. */
+add_theme_support( 'hybrid-core-menus', array( 'primary', 'subsidiary' ) );
+//add_theme_support( 'hybrid-core-sidebars', array( 'primary' ) );
+add_theme_support( 'hybrid-core-widgets' );
+add_theme_support( 'hybrid-core-shortcodes' );
+add_theme_support( 'hybrid-core-theme-settings', array( 'about', 'footer' ) );
+add_theme_support( 'hybrid-core-drop-downs' );
+add_theme_support( 'hybrid-core-template-hierarchy' );
+
+/* Add theme support for framework extensions. */
+add_theme_support( 'theme-layouts', array( '1c', '2c-l', '2c-r' ) );
+//add_theme_support( 'post-stylesheets' );
+//add_theme_support( 'dev-stylesheet' );
+add_theme_support( 'loop-pagination' );
+//add_theme_support( 'get-the-image' );
+add_theme_support( 'breadcrumb-trail' );
+add_theme_support( 'cleaner-gallery' );
+
+
+
 
 ?>
