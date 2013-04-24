f61aed68dbdab2d1995b51ff295aa19c6a00f92f
David <getdavemail@gmail.com>
2013-04-24 16:53:28 +0100

    Added checked-out version of Web Starter Toolkit. Created /assets/ dir

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/**
+ * MAIN JS
+ *
+ * This file is reserved for custom JavaScript.
+ * Use some decent JS code patterns when writing this stuff
+ *
+ * http://addyosmani.com/resources/essentialjsdesignpatterns/book/
+ * 
+ */
+
+
+/* 
+var myRevealingModule = function () {
+
+    var privateVar = "",
+        publicVar  = "";
+
+    function privateFunction() {
+        
+    }
+    
+    function publicFunction() {
+       
+    }
+
+    // Reveal public pointers to  
+    // private functions and properties
+
+    return {
+        privateMethod: privateFunction,
+        publicMethod: publicFunction
+    };
+}(); */
+
+
+
+// Capture jQuery in noConflict mode and retranslate to $ alias
+(function($) {
+
+
+}(jQuery));
