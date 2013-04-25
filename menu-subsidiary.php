f35c9b79b97f471c5d5a4868ea59c863c0cac964
David <getdavemail@gmail.com>
2013-04-25 11:00:16 +0100

    Removed grid classes. Fixes #24

--- menu-subsidiary.php
+++ menu-subsidiary.php
@@ -1,5 +1,5 @@
 <?php if ( has_nav_menu( 'subsidiary' ) ) : ?>
 
-	<?php wp_nav_menu( array( 'theme_location' => 'subsidiary', 'container' => 'div', 'container_class' => 'menu menu-subsidiary firstcol size1of2', 'menu_class' => 'nav nav-subsidiary', 'menu_id' => 'nav-subsidiary', 'fallback_cb' => '' ) ); ?>
+	<?php wp_nav_menu( array( 'theme_location' => 'subsidiary', 'container' => 'div', 'container_class' => 'menu menu-subsidiary', 'menu_class' => 'nav nav-subsidiary', 'menu_id' => 'nav-subsidiary', 'fallback_cb' => '' ) ); ?>
 
 <?php endif; ?>
