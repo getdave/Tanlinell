6694ce0402df496098fe673b5874fe75d31ab208
David <getdavemail@gmail.com>
2013-04-26 14:58:30 +0100

    Created Grunt.js file. Commented out rsync deploy section

---
+++ Gruntfile.js
@@ -0,0 +1,119 @@
+'use strict';
+module.exports = function(grunt) {
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+        // watch for changes and trigger compass, jshint, uglify and livereload
+        watch: {
+            compass: {
+                files: ['assets/scss/**/*.{scss,sass}'],
+                tasks: ['compass']
+            },
+            js: {
+                files: '<%= jshint.all %>',
+                tasks: ['jshint', 'uglify']
+            },
+            livereload: {
+                files: ['*.css', 'assets/js/*.js', '*.html', '*.php', 'assets/images/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+                tasks: ['livereload']
+            }
+        },
+
+        // compass and scss
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb',
+                    force: true
+                }
+            }
+        },
+
+        // javascript linting with jshint
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                "force": true
+            },
+            all: [
+                'Gruntfile.js',
+                'assets/js/source/**/*.js'
+            ]
+        },
+
+        // uglify to concat, minify, and make source maps
+        uglify: {
+            dist: {
+                options: {
+                    sourceMap: 'assets/js/map/source-map.js'
+                },
+                files: {
+                    'assets/js/plugins.min.js': [
+                        'assets/js/source/plugins.js',
+                        'assets/js/vendor/**/*.js',
+                        '!assets/js/vendor/modernizr*.js'
+                    ],
+                    'assets/js/main.min.js': [
+                        'assets/js/source/main.js'
+                    ]
+                }
+            }
+        },
+
+        // image optimization
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7,
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/images/',
+                    src: '**/*',
+                    dest: 'assets/images/'
+                }]
+            }
+        },
+
+        // deploy via rsync
+        /* 
+        deploy: {
+            staging: {
+                src: "./",
+                dest: "~/path/to/theme",
+                host: "user@host.com",
+                recursive: true,
+                syncDest: true,
+                exclude: ['.git*', 'node_modules', '.sass-cache', 'Gruntfile.js', 'package.json', '.DS_Store', 'README.md', 'config.rb', '.jshintrc']
+            },
+            production: {
+                src: "./",
+                dest: "~/path/to/theme",
+                host: "user@host.com",
+                recursive: true,
+                syncDest: true,
+                exclude: '<%= rsync.staging.exclude %>'
+            }
+        },
+         */
+        cc: {
+            // catch that comma!
+        }
+
+    });
+
+    // rename tasks
+    grunt.renameTask('regarde', 'watch');
+    grunt.renameTask('rsync', 'deploy');
+
+    // register task
+    grunt.registerTask('default', [
+        'livereload-start',
+        'watch'
+    ]);
+
+};
