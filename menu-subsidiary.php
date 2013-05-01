efdac8a5131b5ca6d6973b1b2814419b81d914e5
David <getdavemail@gmail.com>
2013-05-01 13:34:59 +0100

    Added depth param to menu-subsidiary. Fixes #48

--- menu-subsidiary.php
+++ menu-subsidiary.php
@@ -1,5 +1,5 @@
 <?php if ( has_nav_menu( 'subsidiary' ) ) : ?>
 
-	<?php wp_nav_menu( array( 'theme_location' => 'subsidiary', 'container' => 'div', 'container_class' => 'menu menu-subsidiary', 'menu_class' => 'nav nav-subsidiary', 'menu_id' => 'nav-subsidiary', 'fallback_cb' => '' ) ); ?>
+	<?php wp_nav_menu( array( 'theme_location' => 'subsidiary', 'container' => 'div', 'container_class' => 'menu menu-subsidiary', 'menu_class' => 'nav nav-subsidiary', 'menu_id' => 'nav-subsidiary', 'fallback_cb' => '', 'depth' => 1 ) ); ?>
 
 <?php endif; ?>
