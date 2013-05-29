781c95cb80a21e2e9c950045989367fe2845d869
Neil Berry <neil@burfieldcreative.co.uk>
2013-05-29 14:35:33 +0100

    update meta to allow selection from existing pages

--- modules/homepage-slider/slide-link-layout.php
+++ modules/homepage-slider/slide-link-layout.php
@@ -10,11 +10,44 @@
 		<?php $mb->the_field('text'); ?>
 		<span>Link text</span>
 		<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/>
-		
+	</p>
+	<p>
+	
 		<?php $mb->the_field('url'); ?>
-		<span>Link url</span>
+		<span>Custom Link URL</span>
 		<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/>
 		
+		<?php $args = array(
+			'sort_order' => 'ASC',
+			'sort_column' => 'post_title',
+			'hierarchical' => 1,
+			'exclude' => '',
+			'include' => '',
+			'meta_key' => '',
+			'meta_value' => '',
+			'authors' => '',
+			'child_of' => 0,
+			'parent' => -1,
+			'exclude_tree' => '',
+			'number' => '',
+			'offset' => 0,
+			'post_type' => 'page',
+			'post_status' => 'publish'
+		);
+		$pages = get_pages($args);
+		?>
+		
+		<span>Select Page</span>
+		<?php $mb->the_field('page'); ?>
+		<select name="<?php $mb->the_name(); ?>">
+			<option value="">Select...</option>
+			<option value="q"<?php $mb->the_select_state('q'); ?>>q</option>
+			
+			<?php foreach($pages AS $page) : ?>
+				<option value="<?php echo $page->ID; ?>"<?php $mb->the_select_state($page->ID); ?>><?php echo $page->post_title; ?></option>
+			<?php endforeach; ?>
+		</select>
+			
 	</p>
 
 </div>
