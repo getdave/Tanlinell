6f6b3b228224fe21d6fd46a5f3c819178104ff81
Neil Berry <neil@burfieldcreative.co.uk>
2013-06-05 16:30:44 +0100

    clients single

--- modules/clients/clients_single.php
+++ modules/clients/clients_single.php
@@ -16,58 +16,82 @@ get_header(); ?>
 			
 			<?php
 				//setup the custom meta object
-				global $our_team_metabox;
+				global $clients_metabox;
 				
 				// get the meta data for the current post
-				$our_team_metabox->the_meta();
-										
+				$clients_metabox->the_meta();
+				
+							
 				//link meta data
-				$our_team_metabox->the_field('name');
-				$name = $our_team_metabox->get_the_value();
+				$clients_metabox->the_field('client_industry');
+				$client['industry'] = $clients_metabox->get_the_value();
 				
-				$our_team_metabox->the_field('job_title');
-				$job_title = $our_team_metabox->get_the_value();
+				$clients_metabox->the_field('client_website');
+				$client['website'] = $clients_metabox->get_the_value();
 				
-				$our_team_metabox->the_field('email');
-				$email = $our_team_metabox->get_the_value();
+				$clients_metabox->the_field('client_email');
+				$client['email'] = $clients_metabox->get_the_value();
 				
-				$our_team_metabox->the_field('twitter');
-				$twitter = $our_team_metabox->get_the_value();
+				$clients_metabox->the_field('client_phone');
+				$client['phone'] = $clients_metabox->get_the_value();
 				
-				$our_team_metabox->the_field('linkedin');
-				$linkedin = $our_team_metabox->get_the_value();
+				$clients_metabox->the_field('client_facebook');
+				$client['facebook'] = $clients_metabox->get_the_value();
 				
-				$featured_image       =  tanlinell_get_post_thumb( $post->ID );
-				$post_thumbnail_sized =  trailingslashit(get_stylesheet_directory_uri()) . 'timthumb.php?src='.$featured_image[0] . '&q=80&w=900&zc=1';
-									
-				$departments_roles = wp_get_post_terms($post->ID, 'departments_roles', array("fields" => "names"));
-			
+				$clients_metabox->the_field('client_twitter');
+				$client['twitter'] = $clients_metabox->get_the_value();
+				
+				$clients_metabox->the_field('client_linkedin');
+				$client['linkedin'] = $clients_metabox->get_the_value();
+				
+				
+				$post_thumbnail_sized	=  tanlinell_get_post_thumb( $post->ID , array( 'width' => 844, 'height' => 494, 'crop' => true, 'resize' => true ));							
+				
+				//get the alt text
+				$featured_image_alt = trim(strip_tags( get_post_meta(get_post_thumbnail_id( $post->ID ), '_wp_attachment_image_alt', true) ));
+				if(empty($featured_image_alt))
+					$featured_image_alt = 'Image for '.ucwords(get_the_title()); //defaults if none found
+								
+				$service_types = wp_get_post_terms($post->ID, 'service_types', array("fields" => "slugs")); 
+				
 			?>
 		
 			<div class="grid-wrap gc">
-				<div class="img-polaroid gc mbl d1-3">
-					<img src="<?php echo $post_thumbnail_sized; ?>" alt="" style="">
+				<div class="img-polaroid gc mbl d1-4">
+					<a href="<?php echo get_permalink() ?>">
+						<img src="<?php echo $post_thumbnail_sized[0]; ?>" alt="<?php echo $featured_image_alt; ?>">
+					</a>
 				</div>
-				<div class="gc d2-3">
+				<div class="gc d3-4">
 					
-					<h3><?php echo ucwords($name); ?></h3>
-					<h4><?php echo $job_title; ?></h4>
+					<h3><a href="<?php echo get_permalink() ?>"><?php the_title(); ?></a></h3>
+					
 					<h5>
 					<?php
-					foreach($departments_roles AS $slug) :
-					$term = get_term_by('slug', $slug, 'departments_roles')
+					foreach($service_types AS $slug) :
+					$term = get_term_by('slug', $slug, 'service_types')
 					?>
-					<a href="<?php echo get_term_link($term->slug,'departments_roles');?>"><?php echo $term->name; ?></a>								
+					<a href="<?php echo get_term_link($term->slug,'service_types');?>"><?php echo $term->name; ?></a>								
 					<?php endforeach; ?>
 					</h5>
-					<div class="grid-wrap">
-						<h5 class="gc d1-3"><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></h5>
-						<h5 class="gc d1-3"><a href="<?php echo $twitter; ?>" target="_blank"><?php echo $twitter; ?></a></h5>
-						<h5 class="gc d1-3"><a href="<?php echo $linkedin; ?>" target="_blank"><?php echo $linkedin; ?></a></h5>
-					</div>
+					
+					<h4><?php the_excerpt(); ?></h4>
+					
+					<ul>
+						<?php 
+						foreach ($client AS $c) : 
+							if(!empty($c)) :
+						?>								
+						<li><?php echo $c; ?></li>
+						<?php 
+							endif;
+						endforeach;
+						?>
+					</ul>
+					
 					<?php the_content(); ?>
+					
 			   </div>
-			</div>
 			
 		<?php endwhile; // end of the loop. ?>
 		
