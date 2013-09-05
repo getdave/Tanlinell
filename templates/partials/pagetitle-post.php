3f0f2697cf1519f452519b094ba5808f9453d2d1
Neil Berry <neil@burfieldcreative.co.uk>
2013-09-05 16:51:06 +0100

    Update pagetitle templates

--- templates/partials/pagetitle-post.php
+++ templates/partials/pagetitle-post.php
@@ -1,33 +1,34 @@
 <?php
-	//setup the custom meta object
-	global $global_metabox;
-	
-	// get the meta data for the current post
-	$global_metabox->the_meta();
-	
-	//link meta data
-	$global_metabox->the_field('page_title');
-	$page_title = esc_html( $global_metabox->get_the_value() );
-	
-	$global_metabox->the_field('page_subtitle');
-	$page_subtitle = esc_html( $global_metabox->get_the_value() );
-	
-	//if $page_match == true/1 -> we dont need to link this
-	$page_match = ( trailingslashit(home_url( $wp->request ))==get_permalink() ) ? true : false;
+//setup the custom meta object
+global $global_metabox;
+
+// get the meta data for the current post
+$global_metabox->the_meta();
+
+//link meta data
+$global_metabox->the_field('page_title');
+$page_title = esc_html( $global_metabox->get_the_value() );
+
+$global_metabox->the_field('page_subtitle');
+$page_subtitle = esc_html( $global_metabox->get_the_value() );
+
+//if $page_match == true/1 -> we dont need to link this
+$page_match = ( trailingslashit(home_url( $wp->request ))==get_permalink() ) ? true : false;
 ?>
 <header class="title-block entry-header">
 
 	<?php if(true == $page_match) : ?>
-	<h1 class="title-block__heading entry-title" itemprop="headline">		
-		
+	
+	<h1 class="title-block__heading entry-title" itemprop="headline">
 		<?php if ( $page_title ) : ?>
 			<?php echo $page_title; ?>
 		<?php else : ?>
 			<?php the_title(); ?>
-		<?php endif; ?>
-		
+		<?php endif; ?>		
 	</h1>
+	
 	<?php else: ?>
+	
 	<h2 class="title-block__heading entry-title" itemprop="headline">		
 		<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'tanlinell' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
 		
@@ -39,8 +40,8 @@
 
 		</a>
 	</h2>
-	<?php endif; ?>
 	
+	<?php endif; ?>	
 	
 	<?php if ( $page_subtitle ) : ?>
 	<p class="page-subtitle"><?php echo $page_subtitle ?></p>
