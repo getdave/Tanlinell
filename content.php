fc206b0365f8a3b5793649d352755582e7e35000
David <getdavemail@gmail.com>
2013-09-09 12:13:53 +0100

    Refactored pagetitle for post templates to use betetr HTML semantics. Removed duplication of entry-meta by moving footer element from content and into page title,

--- content.php
+++ content.php
@@ -29,35 +29,5 @@
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
-	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'tanlinell' ) );
-				if ( $categories_list && tanlinell_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'tanlinell' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'tanlinell' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="sep"> | </span>
-			<span class="tag-links">
-				<?php printf( __( 'Tagged %1$s', 'tanlinell' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="sep"> | </span>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'tanlinell' ), __( '1 Comment', 'tanlinell' ), __( '% Comments', 'tanlinell' ) ); ?></span>
-		<?php endif; ?>
-
-		<?php edit_post_link( __( 'Edit', 'tanlinell' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-meta -->
+	
 </article><!-- #post-<?php the_ID(); ?> -->
--- templates/partials/pagetitle-post.php
+++ templates/partials/pagetitle-post.php
@@ -15,23 +15,23 @@ $page_subtitle = esc_html( $global_metabox->get_the_value() );
 //if $page_match == true/1 -> we dont need to link this
 $page_match = ( trailingslashit(home_url( $wp->request ))==get_permalink() ) ? true : false;
 ?>
-<header class="title-block entry-header">
+<header class="entry-header">
 
 	<?php if(true == $page_match) : ?>
-	
-	<h1 class="title-block__heading entry-title" itemprop="headline">
+
+	<h1 class="entry-title" itemprop="headline">
 		<?php if ( $page_title ) : ?>
 			<?php echo $page_title; ?>
 		<?php else : ?>
 			<?php the_title(); ?>
-		<?php endif; ?>		
+		<?php endif; ?>
 	</h1>
-	
+
 	<?php else: ?>
-	
-	<h2 class="title-block__heading entry-title" itemprop="headline">		
+
+	<h2 class="entry-title" itemprop="headline">
 		<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'tanlinell' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
-		
+
 		<?php if ( $page_title ) : ?>
 			<?php echo $page_title; ?>
 		<?php else : ?>
@@ -40,14 +40,40 @@ $page_match = ( trailingslashit(home_url( $wp->request ))==get_permalink() ) ? t
 
 		</a>
 	</h2>
-	
-	<?php endif; ?>	
-	
-	<?php if ( $page_subtitle ) : ?>
-	<p class="page-subtitle"><?php echo $page_subtitle ?></p>
-	<?php endif; ?>	
-	
-	<div class="title-block__meta entry-meta" itemprop="dateCreated">
-		<?php tanlinell_posted_on(); ?>
-	</div><!-- .entry-meta -->	
+
+	<?php endif; ?>
 </header>
+
+
+<footer class="entry-meta title-block__meta">
+	<span itemprop="dateCreated"><?php tanlinell_posted_on(); ?></span>
+	<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+		<?php
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( __( ', ', 'tanlinell' ) );
+			if ( $categories_list && tanlinell_categorized_blog() ) :
+		?>
+		<span class="cat-links">
+			<?php printf( __( 'in %1$s', 'tanlinell' ), $categories_list ); ?>
+		</span>
+		<?php endif; // End if categories ?>
+
+		<?php
+			/* translators: used between list items, there is a space after the comma */
+			$tags_list = get_the_tag_list( '', __( ', ', 'tanlinell' ) );
+			if ( $tags_list ) :
+		?>
+		<span class="tag-links">
+			<?php printf( __( 'Tagged %1$s', 'tanlinell' ), $tags_list ); ?>
+		</span>
+		<?php endif; // End if $tags_list ?>
+	<?php endif; // End if 'post' == get_post_type() ?>
+
+	<?php if ( ! post_password_required() && ( comments_open() ) ) : ?>
+	<span class="sep"> | </span>
+	<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'tanlinell' ), __( '1 Comment', 'tanlinell' ), __( '% Comments', 'tanlinell' ) ); ?></span>
+	<?php endif; ?>
+</footer><!-- .entry-meta -->
+
+
+
