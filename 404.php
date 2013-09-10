63eac518cb7a1ac2bbf8d7b04f37b37856d16463
David <getdavemail@gmail.com>
2013-09-10 09:56:48 +0100

    Update markup structure on 404 template to ensure layout is correct. Fixes #158

--- 404.php
+++ 404.php
@@ -8,27 +8,21 @@
 
 get_header(); ?>
 
-<div class="main" role="main">
+<div class="column-container">
+	<div class="main" role="main">
+		<article id="post-0" class="post error404 not-found">
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'Sorry, but that page can&rsquo;t be found.', 'tanlinell' ); ?></h1>
+			</header><!-- .entry-header -->
 
-	<article id="post-0" class="post error404 not-found">
-		<header class="entry-header">
-			<h1 class="entry-title"><?php _e( 'Sorry, but that page can&rsquo;t be found.', 'tanlinell' ); ?></h1>
-		</header><!-- .entry-header -->
+			<div class="entry-content">
+				<p><?php _e( 'It looks like nothing was found at this location. Perhaps you could try the <a href="/sitemap/" title="View the website sitemap">sitemap</a>.', 'tanlinell' ); ?></p>
 
-		<div class="entry-content">
-			<p><?php _e( 'It looks like nothing was found at this location. Perhaps you could try one of the links below or a search?', 'tanlinell' ); ?></p>
-
-			<?php get_search_form(); ?>
-
-			
-
-			
-
-		</div><!-- .entry-content -->
-	</article><!-- #post-0 .post .error404 .not-found -->
-</div><!-- .main -->
-<div class="sub">
-<?php get_sidebar(); ?>
+			</div><!-- .entry-content -->
+		</article><!-- #post-0 .post .error404 .not-found -->
+	</div><!-- .main -->
+	<div class="sub">
+		<?php get_sidebar(); ?>
+	</div>
 </div>
-
 <?php get_footer(); ?>
