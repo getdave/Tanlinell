0c862ddb5f7c691ffc19a2d72bb05933943daa1b
David <getdavemail@gmail.com>
2013-09-10 09:56:23 +0100

    Fix sidebar descendants file to check for $post before trying to get the ID of the post. Fixes error on 404 page.

--- templates/partials/sidebar-descendants.php
+++ templates/partials/sidebar-descendants.php
@@ -2,24 +2,30 @@
 /**
  * Descendant Pages List Navigation
  */
-$curr_pageid 	= get_the_ID(); 
-$root_parent_id = tanlinell_get_root_parent( $curr_pageid, 'page');
 
-$args = array(
-	'title_li' => false,
-	'echo' => 0,
-	'child_of' => $root_parent_id,
-);
-$descendants_list = wp_list_pages( $args );
+global $post;
 
-$section_title = (get_the_title($root_parent_id)) ? get_the_title($root_parent_id) : 'In this section';
-?>
-	
-<?php if( $descendants_list ) : ?>
-<aside class="menu-sub sidebar">
-	<h4 class="menu-sub__heading"><?php echo ucwords(esc_html($section_title)); ?></h4>
-	<ul class="nav-sub">
-	<?php echo $descendants_list; ?>
-	</ul>
-</aside>
-<?php endif; ?>
+if ($post) { // if there is no post (ie 404 page) then bail out
+	$curr_pageid 	= $post->ID;
+	$root_parent_id = tanlinell_get_root_parent( $curr_pageid, 'page');
+
+	$args = array(
+		'title_li' 	=> false,
+		'echo' 		=> 0,
+		'child_of' 	=> $root_parent_id,
+	);
+	$descendants_list = wp_list_pages( $args );
+
+	$section_title = (get_the_title($root_parent_id)) ? get_the_title($root_parent_id) : 'In this section';
+	?>
+		
+	<?php if( $descendants_list ) : ?>
+	<aside class="menu-sub sidebar">
+		<h4 class="menu-sub__heading"><?php echo ucwords(esc_html($section_title)); ?></h4>
+		<ul class="nav-sub">
+		<?php echo $descendants_list; ?>
+		</ul>
+	</aside>
+	<?php endif; ?>
+
+<?php } ?>
