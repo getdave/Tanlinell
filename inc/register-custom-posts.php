9419a51118313a2a8f62124caf019c2ee8e23b3f
Neil Berry <neil@burfieldcreative.co.uk>
2013-10-22 10:59:52 +0100

    Update custom data for titles and slides to use Custom-meta not wpalchemy

--- inc/register-custom-posts.php
+++ inc/register-custom-posts.php
@@ -10,9 +10,4 @@
 
 
 
-/**
- * Homepage Slides 
- *
- * Custom post for feature slider commonly implemented on the Homepage of the website.
- */
-require( get_template_directory() . '/modules/homepage-slider/register-cpt.php' );
+?>
