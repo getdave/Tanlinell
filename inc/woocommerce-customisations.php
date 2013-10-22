5d2f4ae736e72e6d755ad1bbae9868361135acde
David <getdavemail@gmail.com>
2013-10-22 15:52:27 +0100

    Fix broken html markup on cart template. Only show Hybrid core breadcrumbs on non-WooCommerce templates

--- inc/woocommerce-customisations.php
+++ inc/woocommerce-customisations.php
@@ -11,6 +11,8 @@ if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins',
 	return;
 }
 
+define('WOOCOMMERCE_ACTIVE', true);
+
 
 // Define WooCommerce support
 add_theme_support( 'woocommerce' );
@@ -18,6 +20,7 @@ add_theme_support( 'woocommerce' );
 
 /**
  * WOOCOMMERCE WRAPPERS
+ * 
  * defines the wrapper markup for WooCommerce templates
  */
 
@@ -43,6 +46,7 @@ function tanlinell_wrapper_end() {
 
 /**
  * SHOP LOOP ITEMS
+ * 
  * amend markup of the loop for the Shop listing template
  */
 
@@ -59,24 +63,46 @@ function tanlinell_woocommerce_after_shop_loop_item() {
 
 
 
-
+/**
+ * MANIPULATE CART TABLE HTML
+ * 
+ * alter the core markup of the Cart table in order to break out the coupons and
+ * call to actions from the main table. 
+ */
 add_action('woocommerce_before_cart_table','tanlinell_woocommerce_before_cart_table');
-                        function tanlinell_woocommerce_before_cart_table() {
-                                echo '<div class="cart__contents scrollable"><div>';
-                        }
+function tanlinell_woocommerce_before_cart_table() {
+    $html =  '<div class="cart__contents scrollable">'; 
+    $html .= '<div>';
+
+    echo $html;
+}
 
 
-                        add_action('woocommerce_after_cart_table','tanlinell_woocommerce_after_cart_table');
-                        function tanlinell_woocommerce_after_cart_table() {
-                                echo '</div></div>';
-                        }
+add_action('woocommerce_cart_contents','tanlinell_woocommerce_cart_contents');
+function tanlinell_woocommerce_cart_contents() {
+    $html =  '</tbody></table>'; // prematurely close the cart table because we don't want our "actions" area within a <table>
+    $html .= '</div></div>'; // close our "scrollable" wrapper <div>'s
+    $html .= '<table class="cart__actions"><tbody>'; // open a new table. We have to use a table because we have no control over the closing </table>
+
+    echo $html;
+}
 
 
-                        add_action('woocommerce_cart_contents','tanlinell_woocommerce_cart_contents');
-                        function tanlinell_woocommerce_cart_contents() {
-                                echo '</tbody></table></div></div><table class="cart__actions">';
-                        }
 
+
+
+
+add_filter( 'woocommerce_breadcrumb_defaults', 'tanlinell_woocommerce_breadcrumbs' );
+function tanlinell_woocommerce_breadcrumbs() {
+    return array(
+        'delimiter'   => '<span class="sep">&#47;</span>',
+        'wrap_before' => '<nav class="woocommerce-breadcrumb" itemprop="breadcrumb">',
+        'wrap_after'  => '</nav>',
+        'before'      => '<span>',
+        'after'       => '</span>',
+        'home'        => _x( 'Home', 'breadcrumb', 'woocommerce' ),
+    );
+}
 
 
 
