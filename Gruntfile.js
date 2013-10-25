7c532e29c0a9b7b4c0dad76e2a8f0f2f6ce7872b
David <getdavemail@gmail.com>
2013-10-25 15:40:26 +0100

    Fixed missing comma

--- Gruntfile.js
+++ Gruntfile.js
@@ -228,7 +228,7 @@ module.exports = function(grunt) {
                 tagMessage: 'Version %VERSION%',
                 push: false,
             }
-        }
+        },
 
         cc: {
             // catch that comma!
