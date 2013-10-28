42734e6b803dfdede76b1fa4a4a9c22b4cdf1a19
Neil Berry <neil@burfieldcreative.co.uk>
2013-10-28 14:29:29 +0000

    fix(breadcrumbs): fix fatal error on woocommerece check
    If woocommerce was not installed, Tanlinell header.php threw a fatal
    error.
    
    Closes #218

--- header.php
+++ header.php
@@ -79,5 +79,10 @@ if ($google_company_page_url) :
 	</header><!-- #masthead .site-header -->
 	<?php do_atomic( 'after_banner' ); ?>
 	<div id="content" class="<?php echo apply_atomic( 'site_content_class', 'container site-content ' ); ?>">
-		<?php if ( current_theme_supports( 'breadcrumb-trail' ) && !is_woocommerce() ) breadcrumb_trail(); ?>
+	
+		<?php if( !function_exists('is_woocommerce') || !is_woocommerce()) : ?>
+		
+			<?php if ( current_theme_supports( 'breadcrumb-trail' ) ) breadcrumb_trail(); ?>
+			
+		<?php endif; ?>
 	
