8885e7e871447516ed01fab91bbd3a60f4199e2c
Neil Berry <neil@burfieldcreative.co.uk>
2013-11-22 12:52:02 +0000

    fix(cpt_menu) Ensure cpt items in the menu will have current-page-item class when viewed

---
+++ inc/cpt-current-menu-item.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Current Menu Item for CPT's
+ * Adds the 'current-menu-item' class to menu items when viewing cpt's 
+ */
+function current_type_nav_class($classes, $item) {
+    
+    $post_type = get_post_type();
+    $post_type_obj = get_post_type_object($post_type);
+    
+    if ($item->title == $post_type_obj->label) {
+    	    	
+        array_push($classes, 'current-menu-item');
+        
+    };
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'current_type_nav_class', 10, 2 );
+?>
