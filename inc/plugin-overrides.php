444780bae45e2fbb538ccc5401de8c89abfe1c3f
Neil Berry <neil@burfieldcreative.co.uk>
2014-03-05 08:35:45 +0000

    Addition of GF Button markup in plugins-overrides.php Closes #288

--- inc/plugin-overrides.php
+++ inc/plugin-overrides.php
@@ -12,4 +12,29 @@
  */
 add_filter( 'tablepress_use_default_css', '__return_false' );
 
+
+/**
+ * Filter the Gravity Forms button type to provide custom markup
+ * 
+ * Download button markup requires class provided in form settings
+ */
+function form_submit_button($button, $form){	
+	
+	if( false !== strpos( $form['cssClass'], 'download-form' ) ) {
+		/**
+		 * Requires a class of download form in form settings 
+		 */
+		$button = '<button class="button gform_button btn btn-full btn--append btn--primary" id="gform_submit_button_'.$form["id"].'">
+	       <span class="btn__text">Request Download</span>
+	        <i class="btn__icon btn__icon--arrow icon icon-arrow-down2" aria-hidden="true"></i>
+		</button>';
+	} else {
+		$button = '<button class="button gform_button btn btn-full btn--primary" id="gform_submit_button_'.$form["id"].'">
+			<span class="btn__text">'.$form['button']['text'].'</span>
+		</button>';	
+	}
+	
+    return $button;
+}
+add_filter("gform_submit_button", "form_submit_button", 10, 2);
 ?>
