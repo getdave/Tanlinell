0ac6900d1aacf960723ade956806f848353c7774
David <getdavemail@gmail.com>
2014-03-10 13:54:54 +0000

    Update CSS to match new WooCommerce tempaltes

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	 */
 	 do_action( 'woocommerce_before_single_product' );
 ?>
-
+ddd
 <div itemscope itemtype="http://schema.org/Product" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="product-single__overview">
 		<?php
