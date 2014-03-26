be07b9af5004d1fde5b5a8d6a0b595e6a30630fd
neil@burfieldcreative.co.uk <neil@burfieldcreative.co.uk>
2014-03-26 14:30:54 +0000

    Update menu for CPT #309

--- inc/tanlinell-menu-classes.php
+++ inc/tanlinell-menu-classes.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Current Menu Item
- *
- * Adds the 'current***' class to menu items
+ * Current Menu Item for CPT's
+ * Adds the 'current-menu-item' class to menu items when viewing cpt's 
+ * allows for cpt to be a descendant of a top level menu item
  */
 function tanlinell_current_type_nav_class($classes, $item) {
     
@@ -14,27 +14,25 @@ function tanlinell_current_type_nav_class($classes, $item) {
 	$all_custom_post_types 	= get_post_types( array ( '_builtin' => FALSE ) );
 	$custom_types      		= array_keys( $all_custom_post_types );
 	
-	if ( in_array( $post_type, $custom_types ) ) {
+	if ( is_post_type_archive( get_post_type() ) || is_singular( $post_type ) ) {
 		
-		
-		//if current item is this posts -> post type archive
 		$archive_post = get_page_by_path( $post_type_obj->has_archive );	
-			
-		if ( $archive_post->ID == $item->object_id ) {
-			
+		
+		$ancestors = get_post_ancestors( $archive_post );
+		
+		if( in_array( $item->object_id, $ancestors ) ) {
+		
 	        array_push($classes, 'current-menu-ancestor');
 	        array_push($classes, 'current_page_ancestor');
 	        
 	    }
 		
-		
-		//if current item is the post		
-		if ( $item->object_id == $post->ID ) {
-				
+		if ( $item->title == $archive_post->post_title ) {
+			
 	        array_push($classes, 'current-menu-item');
 	        array_push($classes, 'current_page_item');
 	        
-		}
+	    }
 		
 	}
 	
