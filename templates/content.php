a7e2c0c05be589e91869545c3ba03920e724971c
neil@burfieldcreative.co.uk <neil@burfieldcreative.co.uk>
2014-03-27 18:10:22 +0000

    update pagetitle path

--- templates/content.php
+++ templates/content.php
@@ -5,7 +5,7 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemtype="http://schema.org/BlogPosting">
-	<?php get_template_part( 'templates/partials/pagetitle', 'post' ); ?>
+	<?php get_template_part( 'templates/page-header/pagetitle', 'post' ); ?>
 
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
 	<div class="entry-summary">
