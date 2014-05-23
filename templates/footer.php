13a759b2fd89c1469cbf229f4b0f14440ab35649
neil@burfieldcreative.co.uk <neil@burfieldcreative.co.uk>
2014-05-23 11:24:50 +0100

    Add condition load for bugherd

--- templates/footer.php
+++ templates/footer.php
@@ -43,3 +43,13 @@
 <!-- W3TC-include-js-head -->
 
 <?php wp_footer(); ?>
+<?php if( 'production' != WP_ENV ) : ?>
+<script type='text/javascript'>
+(function (d, t) {
+  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  bh.type = 'text/javascript';
+  bh.src = '//www.bugherd.com/sidebarv2.js?apikey=<%= BUGHERDAPIKEY =%> ';
+  s.parentNode.insertBefore(bh, s);
+  })(document, 'script');
+</script>
+<?php endif; ?>
