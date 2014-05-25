6d4b00095cbe8564e5e52099eafc9b4540ef9c58
neil@burfieldcreative.co.uk <neil@burfieldcreative.co.uk>
2014-05-25 10:05:17 +0100

    Default sidebar display fix for child pages

--- inc-site/tanlinell-sidebar-display.php
+++ inc-site/tanlinell-sidebar-display.php
@@ -11,7 +11,7 @@ add_filter('tanlinell_display_sidebar', 'tanlinell_sidebar_on_special_page');
 function tanlinell_sidebar_on_special_page($sidebar) {
   
   if (	is_page( array( 'sitemap' ) ) ||
-        ( is_page() && false == tanlinell_has_children() ) ||
+        ( is_page() && (false == tanlinell_has_children() && false == $post->post_parent )) ||
   		!have_posts() ) {
     return false;
   }
